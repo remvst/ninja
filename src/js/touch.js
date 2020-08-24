7fb2472859cc8de7dea4054589eb2da6fb9ef2b7
remvst <remvst@gmail.com>
2020-08-24 09:47:47 -0400

    Tweaked mobile controls

--- src/js/touch.js
+++ src/js/touch.js
@@ -5,8 +5,8 @@ ontouchstart = ontouchmove = ontouchend = ontouchcancel = e => {
     for (let i = 0 ; i < e.touches.length ; i++) {
         const x = CANVAS_WIDTH * (e.touches[i].pageX - canvasCoords.left) / canvasCoords.width;
         const buttonIndex = ~~(x / (CANVAS_WIDTH / 4));
-        w.down[KEYBOARD_LEFT] = buttonIndex == 0;
-        w.down[KEYBOARD_RIGHT] = buttonIndex == 1;
-        w.down[KEYBOARD_SPACE] = buttonIndex == 3;
+        down[KEYBOARD_LEFT] = down[KEYBOARD_LEFT] || buttonIndex == 0;
+        down[KEYBOARD_RIGHT] = down[KEYBOARD_RIGHT] || buttonIndex == 1;
+        down[KEYBOARD_SPACE] = down[KEYBOARD_SPACE] || buttonIndex == 2 || buttonIndex == 3;
     }
 };
