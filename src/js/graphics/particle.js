e8eea1cf3e65399836c91e39ef096a5f4243cea2
remvst <remvst@gmail.com>
2020-08-24 09:48:30 -0400

    Don't need dis

--- src/js/graphics/particle.js
+++ src/js/graphics/particle.js
@@ -17,6 +17,5 @@ class Particle {
         R.globalAlpha = this.alpha;
         fs(this.color);
         fillCircle(this.x, this.y, this.size / 2);
-        // fr(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
     }
 }
