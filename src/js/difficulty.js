319511868965d8f5c1a1a9728e14f37913384e3f
remvst <remvst@gmail.com>
2020-08-25 13:32:16 -0400

    Practice mode

--- src/js/difficulty.js
+++ src/js/difficulty.js
@@ -1,7 +1,33 @@
 DIFFICULTY_SETTINGS = [
     // Label, time factor, vision factor
-    NORMAL_DIFFICULTY = {'label': nomangle('NORMAL'), 'timeFactor': 1, 'visionFactor': 1},
-    {'label': nomangle('EASY'), 'timeFactor': 0.8, 'visionFactor': 0.7},
-    {'label': nomangle('SUPER EASY'), 'timeFactor': 0.6, 'visionFactor': 0.5},
-    HARD_DIFFICULTY = {'label': nomangle('HARD'), 'timeFactor': 1, 'visionFactor': 10},
+    NORMAL_DIFFICULTY = {
+        'label': nomangle('NORMAL'),
+        'timeFactor': 1,
+        'visionFactor': 1
+    },
+    {
+        'label': nomangle('EASY'),
+        'timeFactor': 0.8,
+        'visionFactor': 0.7
+    },
+    {
+        'label': nomangle('SUPER EASY'),
+        'timeFactor': 0.6,
+        'visionFactor': 0.5
+    },
+    HARD_DIFFICULTY = {
+        'label': nomangle('HARD'),
+        'timeFactor': 1,
+        'visionFactor': 10
+    }
 ];
+
+// TODO check if coil subscription
+if (true) {
+    DIFFICULTY_SETTINGS.push({
+        'label': nomangle('PRACTICE'),
+        'timeFactor': 1,
+        'visionFactor': 1,
+        'noSpotters': true
+    });
+}
--- src/js/elements/player-spotter.js
+++ src/js/elements/player-spotter.js
@@ -21,6 +21,10 @@ class PlayerSpotter {
     }
 
     get seesPlayer() {
+        if (G.difficulty.noSpotters) {
+            return false;
+        }
+
         // Check if the player is close enough, and within the FOV first
         const angleToPlayer = angleBetween(this, this.level.player);
         const distToPlayer = dist(this, this.level.player);
@@ -42,13 +46,18 @@ class PlayerSpotter {
     }
 
     render() {
+        let visionColor = this.foundPlayer ? '#f00': '#ff0';
+        if (G.difficulty.noSpotters) {
+            visionColor = '#888';
+        }
+
         renderVision(
             this.x,
             this.y,
             this.angle - this.halfFov,
             this.angle + this.halfFov,
             this.appliedMaxDistance,
-            this.foundPlayer ? '#f00': '#ff0'
+            visionColor
         );
     }
 }
