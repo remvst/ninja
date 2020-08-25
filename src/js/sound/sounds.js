c17765fc9b173327acb459a3c1675a63e2545270
remvst <remvst@gmail.com>
2020-08-25 10:15:26 -0400

    Fixed sounds being cut off when played a lot

--- src/js/sound/sounds.js
+++ src/js/sound/sounds.js
@@ -1,8 +1,19 @@
-beepSound = jsfxr([rawFile('config/sounds/beep')]);
-exitSound = jsfxr([rawFile('config/sounds/exit')]);
-failSound = jsfxr([rawFile('config/sounds/fail')]);
-finishSound = jsfxr([rawFile('config/sounds/finish')]);
-jumpSound = jsfxr([rawFile('config/sounds/jump')]);
-landSound = jsfxr([rawFile('config/sounds/land')]);
-nextLevelSound = jsfxr([rawFile('config/sounds/next-level')]);
-notFoundSound = jsfxr([rawFile('config/sounds/not-found')]);
+sound = definition => {
+    const pool = Array(5).fill(0).map(() => jsfxr(definition));
+    return () => {
+        // Cycle the queue by removing the first value and moving it to the last
+        pool.push(pool.shift());
+
+        // Play the sound
+        pool[0]();
+    };
+};
+
+beepSound = sound([rawFile('config/sounds/beep')]);
+exitSound = sound([rawFile('config/sounds/exit')]);
+failSound = sound([rawFile('config/sounds/fail')]);
+finishSound = sound([rawFile('config/sounds/finish')]);
+jumpSound = sound([rawFile('config/sounds/jump')]);
+landSound = sound([rawFile('config/sounds/land')]);
+nextLevelSound = sound([rawFile('config/sounds/next-level')]);
+notFoundSound = sound([rawFile('config/sounds/not-found')]);
