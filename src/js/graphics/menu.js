0c0a132b7eb984bd2cb5e7dc15a335ed004772c1
remvst <remvst@gmail.com>
2020-08-25 15:35:48 -0400

    Not clipping menu on intro

--- src/js/graphics/menu.js
+++ src/js/graphics/menu.js
@@ -10,16 +10,14 @@ class Menu {
     }
 
     animateIn() {
-        const levelWidth = LEVEL_COLS * CELL_SIZE;
-        interp(this, 'titlePosition', -levelWidth / 2, levelWidth / 2, 0.5, 0, easeOutQuint);
-        interp(this, 'subtitlePosition', levelWidth * 3 / 2, levelWidth / 2, 0.5, 1, easeOutQuint);
+        interp(this, 'titlePosition', -CANVAS_WIDTH / 2, LEVEL_WIDTH / 2, 0.5, 0, easeOutQuint);
+        interp(this, 'subtitlePosition', CANVAS_WIDTH * 3 / 2, LEVEL_WIDTH / 2, 0.5, 1, easeOutQuint);
         interp(this, 'dimAlpha', 0, 1, 0.3);
     }
 
     animateOut() {
-        const levelWidth = LEVEL_COLS * CELL_SIZE;
-        interp(this, 'titlePosition', levelWidth / 2, levelWidth * 3 / 2, 0.5, 0, easeInQuint);
-        interp(this, 'subtitlePosition', levelWidth / 2, -levelWidth / 2, 0.5, 0, easeInQuint, () => G.menu = null);
+        interp(this, 'titlePosition', LEVEL_WIDTH / 2, CANVAS_WIDTH * 3 / 2, 0.5, 0, easeInQuint);
+        interp(this, 'subtitlePosition', LEVEL_WIDTH / 2, -CANVAS_WIDTH / 2, 0.5, 0, easeInQuint, () => G.menu = null);
         interp(this, 'dimAlpha', 1, 0, 0.3, 0.2);
     }
 
@@ -28,21 +26,24 @@ class Menu {
     }
 
     render() {
-        const levelWidth = LEVEL_COLS * CELL_SIZE;
+        const LEVEL_WIDTH = LEVEL_COLS * CELL_SIZE;
         const levelHeight = LEVEL_ROWS * CELL_SIZE;
 
         translate(
-            (CANVAS_WIDTH - levelWidth) / 2,
+            (CANVAS_WIDTH - LEVEL_WIDTH) / 2,
             (CANVAS_HEIGHT - levelHeight) / 2
         );
 
-        beginPath();
-        rect(0, 0, levelWidth, levelHeight);
-        clip();
+        if (this.dim) {
+            beginPath();
+            rect(0, 0, LEVEL_WIDTH, levelHeight);
+            clip();
 
-        // Dim
-        R.fillStyle = 'rgba(0,0,0,' + this.dim * this.dimAlpha * 0.8 + ')';
-        fr(0, 0, levelWidth, levelHeight);
+            // Dim
+            R.fillStyle = 'rgba(0,0,0,' + this.dimAlpha * 0.8 + ')';
+            fr(0, 0, LEVEL_WIDTH, levelHeight);
+        }
+
 
         R.textAlign = 'center';
         R.textBaseline = 'middle';
