433da6f8b5b437c5c45999558480ee974c8a5048
remvst <remvst@gmail.com>
2020-08-25 23:43:54 -0400

    Fixed gamepads on chrome

--- src/js/gamepad.js
+++ src/js/gamepad.js
@@ -3,17 +3,21 @@ gamepads = () => navigator.getGamepads ? navigator.getGamepads() : [];
 isGamepadButtonPressed = buttonIndex => {
     const pads = gamepads();
     for (var i = 0; i < pads.length; i++) {
-        if (pads[i].buttons[buttonIndex].pressed) {
-            return true;
-        }
+        try {
+            if (pads[i].buttons[buttonIndex].pressed) {
+                return true;
+            }
+        } catch (e) {}
     }
 };
 
 isGamepadAxisNearValue = (axisIndex, targetValue) => {
     const pads = gamepads();
     for (var i = 0; i < pads.length; i++) {
-        if (abs(targetValue - pads[i].axes[axisIndex]) < 0.5) {
-            return true;
-        }
+        try {
+            if (abs(targetValue - pads[i].axes[axisIndex]) < 0.5) {
+                return true;
+            }
+        } catch (e) {}
     }
 };
