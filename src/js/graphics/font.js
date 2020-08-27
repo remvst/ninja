ab4022988c6fea15e33f79842647f82901bbdea9
remvst <remvst@gmail.com>
2020-08-27 09:47:24 -0400

    Simplify font

--- src/js/graphics/font.js
+++ src/js/graphics/font.js
@@ -1,17 +1,17 @@
 FONT = createCanvas(1, 1, (c) => {
-    const testText = document.title;
+    // Pick a string that will most likely have a bunch of characters
+    const testText = location;
 
     // Measure the width for a font that we know does not exist
-    c.font = '100pt d';
+    c.font = '99pt d';
     const reference = c.measureText(testText).width;
 
     // Then measure the same width for fonts that we may support
     return [
         nomangle('Impact'),
-        nomangle('Arial Black'),
-        nomangle('Arial')
+        nomangle('Arial Black')
     ].filter(fontName => {
-        c.font = '100pt ' + fontName;
+        c.font = '99pt ' + fontName;
         return c.measureText(testText).width != reference;
     })[0] || nomangle('serif');
 });
