d13beb702dd82c3c6550af39b09274c09f53ac23
remvst <remvst@gmail.com>
2020-08-27 09:51:02 -0400

    Cleanup

--- src/js/graphics/create-pattern.js
+++ src/js/graphics/create-pattern.js
@@ -1,7 +1,10 @@
-const createCanvasPattern = (patternWidth, patternHeight, instructions) => {
+createCanvasPattern = (patternWidth, patternHeight, instructions) => {
     const x = createCanvas(patternWidth, patternHeight, instructions);
     const pattern = x.getContext('2d').createPattern(x, 'repeat');
+
+    // Add some extra properties (background rendering needs to know the size of patterns)
     pattern.width = patternWidth;
     pattern.height = patternHeight;
+
     return pattern;
 };
