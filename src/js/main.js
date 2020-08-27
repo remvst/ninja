885d2362957971eab03ec571bbb6eb4777cd2e45
remvst <remvst@gmail.com>
2020-08-27 10:00:23 -0400

    Nicer code

--- src/js/main.js
+++ src/js/main.js
@@ -22,8 +22,8 @@ onload = () => {
     const loop = (func, nextFrameFunc) => {
         let lastFrame = performance.now();
         const iteration = () => {
-            let n = performance.now(),
-                e = min((n - lastFrame) / 1000, 1000 / 10);
+            const n = performance.now();
+            const e = min((n - lastFrame) / 1000, 1000 / 10);
 
             lastFrame = n;
             func(e, ~~(1 / e));
